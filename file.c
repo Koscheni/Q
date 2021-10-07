620fac899d9b4bde44c9d8d0589f064a9998206d
Onkov Hudanina Yakobi <hydanina@gmail.com>
2021-10-08 00:10:16 +0500

    Give a personalized hello.

--- file.c
+++ file.c
@@ -7,6 +7,7 @@ int main(int argc, char **argv)
 
     printf("Enter your name: ");
     fgets(name, 255, stdin);
+    printf("length = %d\n", strlen(name)); /* debug line */
     name[strlen(name)-1] = '\0'; /* remove the newline at the end */
 
     printf("Hello %s!\n", name);
